820d5392d6b351fe42262d880a7adefe5ecf1ab5
Thien Tue Nguyen <151539442+thntue@users.noreply.github.com>
2026-09-22 18:44:05 +0700

    Time: 32 ms (36.77%), Space: 95.9 MB (15.99%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                maxLength = Math.max(maxLength, currentStreak);
+            }
+        }
+
+        return maxLength;
+    }
+}
