b34fc9a67aa2106ce1ad3a52cf1548de72c6de2e
Thien Tue Nguyen <151539442+thntue@users.noreply.github.com>
2026-09-22 18:41:12 +0700

    Time: 21 ms (99.69%), Space: 43.9 MB (97.04%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Solution {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+    }
+}
